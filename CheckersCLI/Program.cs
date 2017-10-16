ba0f5953f31da277aeb0f2aadbfa7b0f82bbfd87
SamJonesUniversity <32851912+SamJonesUniversity@users.noreply.github.com>
2017-10-16 21:05:24 +0100

    Add files via upload

---
+++ CheckersCLI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckersCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BoardDraw draw = new BoardDraw();
+            int[,] board = new int[4,8];
+
+            int c = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    c++;
+                    if (c == 9 || c == 11 || c == 13 || c == 15 || c == 18 || c == 20 || c == 22 || c == 24)
+                    {
+                        board[i, j] = 0;
+                    }
+                    else if (i <= 1)
+                    {
+                        board[i, j] = 1;
+                    }
+                    else
+                    {
+                        board[i, j] = 2;
+                    }
+                }
+            }
+
+            draw.DrawDefualtBoard();
+            Console.ReadLine();
+            draw.UpdateBoard(board);
+            Console.ReadLine();
+        }
+    }
+}
