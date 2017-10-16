407b3eef39dea4003f8b8157cb1400b36b4c09d1
SamJonesUniversity <32851912+SamJonesUniversity@users.noreply.github.com>
2017-10-17 00:52:35 +0100

    Add files via upload

---
+++ CheckersCLI/BoardArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckersCLI
+{
+    class BoardArray
+    {
+        public Array BoardCreate(int[,] board)
+        {
+            int c = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    c++;
+                    if (c == 9 || c == 11 || c == 13 || c == 15 || c == 18 || c == 20 || c == 22 || c == 24)
+                    {
+                        board[i, j] = 0;
+                    }
+                    else if (i <= 1)
+                    {
+                        board[i, j] = 1;
+                    }
+                    else
+                    {
+                        board[i, j] = 2;
+                    }
+                }
+            }
+            return board;
+        }
+    }
+}
--- CheckersCLI/Program.cs
+++ CheckersCLI/Program.cs
@@ -12,29 +12,9 @@ namespace CheckersCLI
             BoardDraw draw = new BoardDraw();
             int[,] board = new int[4,8];
 
-            int c = 0;
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 8; j++)
-                {
-                    c++;
-                    if (c == 9 || c == 11 || c == 13 || c == 15 || c == 18 || c == 20 || c == 22 || c == 24)
-                    {
-                        board[i, j] = 0;
-                    }
-                    else if (i <= 1)
-                    {
-                        board[i, j] = 1;
-                    }
-                    else
-                    {
-                        board[i, j] = 2;
-                    }
-                }
-            }
+            BoardArray nBoard = new BoardArray();
 
-            draw.DrawDefualtBoard();
-            Console.ReadLine();
+            nBoard.BoardCreate(board);
             draw.UpdateBoard(board);
             Console.ReadLine();
         }
