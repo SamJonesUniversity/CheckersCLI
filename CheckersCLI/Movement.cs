7d42fce1ac7e2b8af6f99d65194ec0dc6dce58df
SamJonesUniversity <32851912+SamJonesUniversity@users.noreply.github.com>
2017-10-18 23:32:29 +0100

    Add files via upload

---
+++ CheckersCLI/Movement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckersCLI
+{
+    class Movement
+    {
+
+        public void firstPoint()
+        {
+            bool passed = false;
+            while (!passed)
+            {
+                Console.WriteLine("Which piece would you like to move?");
+                string[] line = Console.ReadLine().Split(',');
+
+                try
+                {
+                    int[] startCoords = Array.ConvertAll(line, int.Parse);
+                    passed = true;
+                }
+                catch
+                {
+                    Console.WriteLine("That is an invalid co-ordinate");               
+                }
+
+            }
+
+            secondPoint();
+        }
+
+        public void secondPoint()
+        {
+            bool passed = false;
+            while (!passed)
+            {
+                Console.WriteLine("Where will you move this piece?");
+                string[] line = Console.ReadLine().Split(',');
+
+                try
+                {
+                    int[] startCoords = Array.ConvertAll(line, int.Parse);
+                    passed = true;
+                }
+                catch
+                {
+                    Console.WriteLine("That is an invalid co-ordinate");
+                }
+            }
+        }
+    }
+}
--- CheckersCLI/Program.cs
+++ CheckersCLI/Program.cs
@@ -9,14 +9,55 @@ namespace CheckersCLI
     {
         static void Main(string[] args)
         {
-            BoardDraw draw = new BoardDraw();
-            int[,] board = new int[4,8];
-
+            //Initiate Objects
             BoardArray nBoard = new BoardArray();
+            BoardDraw draw = new BoardDraw();
+            Movement move = new Movement();
+            Random RNG = new Random();
 
+            //Initiate Variables
+            int playerTurn = RNG.Next(1, 2); //Change 2 to 3
+            int[,] board = new int[4, 8];
+            bool gameOver = false;
+            int humanPlayer = 0;
+
+            //Create a new board
             nBoard.BoardCreate(board);
-            draw.UpdateBoard(board);
-            Console.ReadLine();
+
+            //
+            while (humanPlayer < 1 || humanPlayer > 2)
+            {
+                Console.WriteLine("Will you be player 1 or player 2? Type 1 or 2 below to answer.");
+
+                int.TryParse(Console.ReadLine(), out humanPlayer);
+
+                Console.Clear();
+
+            }
+
+            while (!gameOver)
+            {
+                if (playerTurn == 1)
+                {
+                        draw.UpdateBoard(board);
+                        Console.WriteLine("Player 1 turn");
+                        move.firstPoint();
+
+                        Console.Clear();
+                        playerTurn = 2;
+                }
+                else
+                {
+                    draw.UpdateBoard(board);
+                    Console.WriteLine("Player 2 turn");
+
+                    Console.ReadLine();
+                    Console.Clear();
+                    playerTurn = 1;
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
